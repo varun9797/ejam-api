051e53a1adc533eb7dc93053bab1bdef8140d8be
varun9797 <verma9797@gmail.com>
2020-09-03 12:02:47 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const password = "D0nt_know";
-mongoose.connect(`mongodb+srv://varunv:${password}@cluster0-iribv.mongodb.net/ejamdb?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(``, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
